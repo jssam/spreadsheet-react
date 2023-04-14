62be339ba37a994580978a47826dd753a6ad10db
sanyam.jain <sanyam.jain@grabpay.com>
2023-04-14 18:45:33 +0530

    vault credential for fico

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { useState } from 'react'
 import Spreadsheet from './components/Spreadsheet'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import DataTable from './Datasheet';
 
 
+const rows = []
+const positions = []
 
+for (let i = 0; i < 10; i++) {
+  rows.push({
+    id: i,
+    firstName: 'First name ' + i,
+    secondName: 'Second name ' + i,
+    positionId: 3,
+    age: i,
+  })
+}
+
+for (let i = 1; i < 6; i++) {
+  positions.push({
+    id: i,
+    name: 'Long Position Name ' + i,
+  })
+}
 const App = () => {
-  return (
+  return (<>
     <Spreadsheet />
+    <DataTable rows={rows} focusOnSingleClick={true} isScrollable />
+    </>
   );
 }
 
---
+++ src/comp/index.js
@@ -0,0 +1,5 @@
+import Grid from './scrollWrapper'
+import Input from './input'
+import Select from './select'
+
+export { Grid, Input, Select }
